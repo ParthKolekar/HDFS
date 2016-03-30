d48fdfa65dfe67a5ed22ba8450b62ca02e692785
Shrenik <shrenik.jain@research.iiit.ac.in>
2016-03-31 02:12:59 +0530

    Done something in client

--- Client/Client.java
+++ Client/Client.java
@@ -1,6 +1,21 @@
 package Client;
 
+import java.awt.List;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import NameNode.INameNode;
+import Protobuf.HDFSProtobuf.BlockLocationRequest;
+import Protobuf.HDFSProtobuf.CloseFileRequest;
+import Protobuf.HDFSProtobuf.CloseFileResponse;
+import Protobuf.HDFSProtobuf.OpenFileRequest;
+import Protobuf.HDFSProtobuf.OpenFileResponse;
 
 
 public class Client {
@@ -11,12 +26,51 @@ public class Client {
 		super();
 	}
 	
-	private  static void put(String fileName) {
-		
-		
+	private  static void put(String fileName) throws MalformedURLException, RemoteException, NotBoundException {
+		try {
+			OpenFileRequest.Builder openFileRequest = OpenFileRequest.newBuilder();
+			openFileRequest.setFileName(fileName);
+			openFileRequest.setForRead(true);
+			byte[] serializedOpenFileRequest = openFileRequest.build().toByteArray();
+			INameNode nameNode = (INameNode)Naming.lookup("NameNode");
+			byte [] serializedOpenFileResponse = null;
+			serializedOpenFileResponse = nameNode.openFile(serializedOpenFileRequest);
+			OpenFileResponse openFileResponse = OpenFileResponse.parseFrom(serializedOpenFileRequest);
+			Integer openStatus = openFileResponse.getStatus();
+			if(openStatus == 0)
+				throw new FileNotFoundException();
+			Integer handle = openFileResponse.getHandle();
+			//Highly doubtful
+			Integer[] blockNums = openFileResponse.getBlockNumsList().toArray(new Integer[0]);
+			//I am not sure what to do here. pdf says to use loop but here we can send entire arrays
+			for (Integer block : blockNums) {
+				BlockLocationRequest.Builder blockLocationRequest = BlockLocationRequest.newBuilder();
+				blockLocationRequest.addAllBlockNums(openFileResponse.getBlockNumsList());
+				
+			}
+			CloseFileRequest.Builder closeFileRequest = CloseFileRequest.newBuilder();
+			closeFileRequest.setHandle(handle);
+			byte [] serializedCloseFileRequest = closeFileRequest.build().toByteArray();
+			byte [] serializedCloseFileResponse = null;
+			serializedCloseFileResponse = nameNode.closeFile(serializedCloseFileRequest);
+			CloseFileResponse closeFileResponse = CloseFileResponse.parseFrom(serializedCloseFileResponse);
+			Integer closeStatus = closeFileResponse.getStatus();
+			if (closeStatus == 0) 
+				throw new IOException();
+						
+			
+		} catch (Exception e) {
+			System.err.println(e);
+			// TODO: handle exception
+		}
 	}
 	
 	private  static void get(String fileName) {
+		OpenFileRequest.Builder openFileRequest = OpenFileRequest.newBuilder();
+		openFileRequest.setFileName(fileName);
+		openFileRequest.setForRead(false);
+		byte[] serializedOpenFileRequest = openFileRequest.build().toByteArray();
+
 		
 		
 	}
@@ -35,17 +89,14 @@ public class Client {
 		switch (type) {
 			case "list":
 				try {
-					list();
-					
+					list();	
 				} catch (Exception e) {
 					System.err.println("list");
 				}
-				
 				break;
 			case "get":
 				try {
-					get(argumentList[1]);
-					
+					get(argumentList[1]);	
 				} catch (Exception e) {
 					System.err.println("get");
 					}
@@ -54,7 +105,7 @@ public class Client {
 				try {
 					put(argumentList[1]);					
 				}
-				catch(IndexOutOfBoundsException e) {
+				catch(Exception e) {
 					System.err.println("Wrong number of arguments specified\n");					
 				}
 				break;
